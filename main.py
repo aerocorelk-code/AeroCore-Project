df71b93a114261aabe1c858de17ec56f5b20913b
aerocorelk-code <aerocore.lk@gmail.com>
2026-03-25 09:47:55 +0530

    Create main.py
    Initial commit of AeroCore main system code

---
+++ main.py
@@ -0,0 +1,7 @@
+# AeroCore Smart Power System v1.0
+print("---------------------------------")
+print("   AEROCORE SYSTEM ONLINE       ")
+print("---------------------------------")
+print("Status: Monitoring Energy...")
+print("Goal: World's #1 Power Card")
+print("Developer: Manisha")
